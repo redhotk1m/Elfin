ddf9526c217c8e08ae6a4cda8ef2eb809c6aa09a
redhotk1m <kim@adsl.no>
2020-03-10 13:29:37 +0100

    Fikset API for innhenting av bilinfo. Må implementere hvordan denne dataen skal håndteres.

--- app/src/main/java/com/example/elfin/API/CarInfoAPI.java
+++ app/src/main/java/com/example/elfin/API/CarInfoAPI.java
@@ -9,58 +9,62 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class CarInfoAPI extends AsyncTask<String, Void, String> {
+
     @Override
     protected String doInBackground(String... strings) {
-
-        String biltemaURL_End = "/2/nb",
-                biltemaURL_Start = "https://reko.biltema.com/v1/Reko/extendedcarinfo/",
-                regNR = strings[0],
-                 jsonFromAPI = "", //getJSONFromURL(biltemaURL_Start,regNR, biltemaURL_End)
-                chassieNR = null;
         try {
-            JSONObject biltemaJSON = new JSONObject(jsonFromAPI);
-            chassieNR = biltemaJSON.getString("chassieNumber");
-        } catch (JSONException e){
-            return "Error";
+            JSONObject biltemaJSON = getJSONFromURL(strings[0]);
+            String chassieNR = biltemaJSON.getString("chassieNumber");
+            //vegvesen API returns JSONObject within JSONArray within JSONObject.
+            JSONObject vegvesenJSON = getJSONFromURL(chassieNR)
+                    .getJSONArray("entries")
+                    .getJSONObject(0);
+            return vegvesenJSON.getString("reg_aar");
+        } catch (JSONException | NullPointerException | IOException e){
+            e.printStackTrace();
+            //Something went wrong, cannot find the correct car
+            return "error";
         }
-
-
-
-        return null;
     }
 
-    JSONObject getJSONFromURL(String identifier){
+    private JSONObject getJSONFromURL(String identifier) throws JSONException, IOException {
         int minimumChassieNumberLength = 17;
-        //Checks if the identifier is a chassienumber or registrationnumber.
-        //It's chassieNR if length >= 17, else it's a regNR
-        //Creates a corresponding URL to the correct service
+        /*
+        Checks if the identifier is a chassienumber or registrationnumber.
+        if length >= 17, it's a chassienumber, else it's a regNR
+        Creates a corresponding URL to the correct service
+         */
         String serviceURL = identifier.length() >= minimumChassieNumberLength ?
-                "https://reko.biltema.com/v1/Reko/extendedcarinfo/" + identifier + "/2/nb"
-                : "https://hotell.difi.no/api/json/vegvesen/utek?query=" + identifier;
-        try {
+                "https://hotell.difi.no/api/json/vegvesen/utek?query=" + identifier
+                : "https://reko.biltema.com/v1/Reko/extendedcarinfo/" + identifier + "/2/nb";
             URL url = new URL(serviceURL);
             HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-            try {
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-                StringBuilder stringBuilder = new StringBuilder();
-                String line;
-                while ((line = bufferedReader.readLine()) != null) {
-                    stringBuilder.append(line).append("\n");
-                }
-                bufferedReader.close();
-                return new JSONObject(stringBuilder.toString());
-            } catch (JSONException e) {
-                e.printStackTrace();
-            } finally {
-                urlConnection.disconnect();
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            StringBuilder stringBuilder = new StringBuilder();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuilder.append(line).append("\n");
             }
-        }catch (IOException e){
-            e.printStackTrace();
-            return null;
+            bufferedReader.close();
+            return new JSONObject(stringBuilder.toString());
+        } finally {
+            urlConnection.disconnect();
         }
-        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String reponse) {
+        if (isError(reponse))
+            return; //Gi beskjed til bruker om at feil har oppstått
+        System.out.println(reponse + " er responsen vi fikk");
+    }
+
+    private boolean isError(String s){
+        return s.toLowerCase().equals("error");
     }
 }
