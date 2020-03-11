abb8237da4f35b47e424bcee2d0dfcfe4de6f456
even996 <evenolsen96@gmail.com>
2020-03-11 14:46:34 +0100

    fikse navngiving

--- app/src/main/java/com/example/elfin/MainActivity.java
+++ app/src/main/java/com/example/elfin/MainActivity.java
@@ -6,8 +6,6 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
-import com.example.elfin.API.CarInfoAPI;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -15,14 +13,15 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Spinner dropdown = findViewById(R.id.spinner);
+        Spinner dropdown = findViewById(R.id.chooseCar);
         //dropdown.setPrompt("EB12342 VW e-Golf");
         String[] items = new String[]{"EB 12342 VW e-Golf", "Legg til bil"};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
         dropdown.setAdapter(adapter);
         dropdown.setPrompt("EB12342 VW e-Golf");
 
-        System.out.println("yoyoyo");
+
+        //swapview
 
 
 
